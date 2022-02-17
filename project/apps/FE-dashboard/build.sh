159161c6c9e86764fd965213795c65be6f88fd84
Monxun <michaelgibney1@gmail.com>
2022-02-17 02:49:52 -0700

    added dockerfiles for fe

--- project/apps/FE-dashboard/build.sh
+++ project/apps/FE-dashboard/build.sh
@@ -1,1 +1,7 @@
 #!/bin/sh
+
+su
+apt update
+apt upgrade -y
+apt install python3-pip -y
+npm install --silent
--- project/apps/FE-landing/build.sh
+++ project/apps/FE-landing/build.sh
@@ -1,1 +1,8 @@
 #!/bin/sh
+
+su
+apt update
+apt upgrade -y
+apt install python3-pip -y
+npm install --silent
+npm install react-scripts@3.4.1 -g --silent
