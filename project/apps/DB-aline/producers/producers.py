bbd1e5e4a31db9053f448476eb4d12a9fe619d8d
Monxun <michaelgibney1@gmail.com>
2022-02-18 00:36:03 -0700

    updated producer scripts

--- project/apps/DB-aline/producers/producers.py
+++ project/apps/DB-aline/producers/producers.py
@@ -1,3 +1,5 @@
+import re
+
 import sqlalchemy
 from sqlalchemy import inspect
 from sqlalchemy.orm import session
@@ -29,10 +31,44 @@ my_session = Session()
 
 def create_applicants(count=100):
     for i in range(count):
+        
+        applicant = fake.profile()
+
+        address = applicant['address']
+        line2 = address.split('\n')[1]
+
+        street = address.split('\n')[0]
+        city, state, zipcode = re.split(', |,| ', line2)
+
+        if applicant['sex'] == 'M':
+            middle_name = fake.name_male().split(' ')[0]
+        else:
+            middle_name = fake.name_female().split(' ')[0]
+
 
         applicant = Applicant(
-            address=fake.address()
+            address=street,
+            city=city,
+            date_of_birth=applicant['birthdate'],
+            drivers_license=''.join(random.choices(string.ascii_uppercase + string.digits, k=8)),
+            email=applicant['email'],
+            first_name=applicant['name'].split(' ')[0],
+            gender=applicant['sex'],
+            income=random.randrange(5000, 500000),
+            last_modified_at=fake.date_between(start_date='-10y', end_date='today'),
+            last_name=applicant['name'].split(' ')[1],
+            mailing_address=street,
+            mailing_city=city,
+            mailing_state=state,
+            mailing_zipcode=zipcode,
+            middle_name=middle_name,
+            phone=fake.phone_number(),
+            social_security=applicant['ssn'],
+            state=state,
+            zipcode=zipcode,
         )
+
+        session.add(applicant)
 
 
 def create_banks(count=10):
