f8972e21df321c99228a198785f7c0f3f9a26071
Monxun <michaelgibney1@gmail.com>
2022-03-03 00:16:12 -0700

    initial

--- project/dev/scripts/build_k8.sh
+++ project/dev/scripts/build_k8.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 
 cd ..
-cd ..
+cd kubernetes
 
 # BUILD DB K8 FILES
-kompose --file ./apps/docker-compose.db.yml convert
-find . -maxdepth 1 -name '*.yml' -exec mv {} ./dev/kubernetes/K8-db/ \;
+cd K8-db
+kompose -f docker-compose.db.yml convert
+cd ..
 
 # BUILD MS K8 FILES
-kompose --file ./apps/docker-compose.ms.yml convert
-find . -maxdepth 1 -name '*.yml' -exec mv {} ./dev/kubernetes/K8-db/ \;
+cd K8-ms
+kompose -f docker-compose.ms.yml convert
+cd ..
 
 # # BUILD FE K8 FILES
-# kompose --file ./apps/docker-compose.fe.yml convert
-# find . -name '*.yml' -exec mv {} ./dev/kubernetes/K8-fe/ \;
+cd K8-fe
+# kompose -f docker-compose.fe.yml convert
+cd ..
 
 # BUILD CI K8 FILES
-kompose --file ./apps/docker-compose.ci.yml convert
-find . -maxdepth 1 -name '*.yml' -exec mv {} ./dev/kubernetes/K8-db/ \;
+cd K8-ci
+kompose -f docker-compose.ci.yml convert
+cd ..
 
 # # BUILD CD K8 FILES
-# kompose --file ./apps/docker-compose.ci.yml convert
-# find . -name '*.yml' -exec mv {} ./dev/kubernetes/K8-cd/ \;
+cd K8-cd
+# kompose -f docker-compose.cd.yml convert
+cd ..
---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# BUILDS AND DEPLOYS ALINE SPRING-BOOT PROJECT
+
+cd project
+echo "STEP-1: DESTROY IMAGES AND OLD NETWORKS"
+source destroy.sh
+echo "STEP-2: BUILD JAR FILES"
+source build.sh
+echo "STEP-3: DEPLOY DB + MS"
+source deploy.
+echo "(SERVICES INITIALIZING... 2 MINUTES)"
+sleep 2 min
+echo "STEP-4: DEPLOY PRODUCERS"
+source cicd.sh
+echo "STEP-5: CREATE KUBERNETES FILES FROM COMPOSE FILES"
+source k8.sh
