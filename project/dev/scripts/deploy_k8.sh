4240c9b19b8bedf6e9fd49a4e0f67dca40dfe49f
Monxun <michaelgibney1@gmail.com>
2022-03-03 09:49:52 +0000

    added k8 deployment

--- project/dev/scripts/deploy_k8.sh
+++ project/dev/scripts/deploy_k8.sh
@@ -1,40 +1,35 @@
 #!/bin/bash
 
-cd dev/kubernetes
+cd ..
+cd kubernetes
 
 # DEPLOY DB K8 PODS
 cd K8-db
-files=( /K8-db/*.yml )
-remove=docker-compose.db.yml
-kubectl apply -f "${files[@]/$remove}"
+kubectl apply -f *.yaml
 cd ..
+
 
 # DEPLOY MS K8 PODS
 cd K8-ms
-files=( /K8-ms/*.yml )
-remove=docker-compose.ms.yml
-kubectl apply -f "${files[@]/$remove}"
+kubectl apply -f *.yaml
 cd ..
+
 
 # # DEPLOY FE K8 PODS
 # cd K8-fe
-# files=( /K8-fe/*.yml )
-# remove=docker-compose.fe.yml
-# kubectl apply -f "${files[@]/$remove}"
+# kubectl apply -f *.yaml
 # cd ..
+
 
 # DEPLOY CI K8 PODS
 cd K8-ci
-files=( /K8-ci/*.yml )
-remove=docker-compose.ci.yml
-kubectl apply -f "${files[@]/$remove}"
+kubectl apply -f *.yaml
 cd ..
+
 
 # # DEPLOY CD K8 PODS
 # cd K8-cd
-# files=( /K8-cd/*.yml )
-# remove=docker-compose.cd.yml
-# kubectl apply -f "${files[@]/$remove}"
+# kubectl apply -f *.yaml
 # cd ..
 
 cd ..
