7d21bce7eab5f125fd04a0e2a1d372fd9f284ce6
Monxun <michaelgibney1@gmail.com>
2022-03-13 21:20:43 -0700

    updated dev

--- project/config/docker.sh
+++ project/config/docker.sh
@@ -15,7 +15,8 @@ chmod +x ./kind
 mv ./kind /root/kind
 
 # PORTAINER
-sudo docker run -d -p 9000:9000 -p 8000:8000 --name PORTAINER --restart always -v /var/run/docker.sock:/var/run/docker.sock -v /srv/portainer:/data portainer/portainer
+docker pull portainer/portainer-ce:latest
+docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
 
 # HELM
 snap install helm --classic
---
+++ project/dev/scripts/local.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+cd ..
+cd ..
+source build.sh
+cd dev
+cd scripts
+source kind_registry.sh
+source build_images.sh
