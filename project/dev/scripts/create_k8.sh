2a62be4a431f5d2660d2b5f79b31b910333133c5
Monxun <michaelgibney1@gmail.com>
2022-03-14 01:51:43 -0700

    updated ingress

--- project/dev/scripts/create_k8.sh
+++ project/dev/scripts/create_k8.sh
@@ -2,8 +2,10 @@
 
 cd ..
 cd ansible
+
 kubectl create -f ./kubernetes/config/namespace-dev.yaml
 kubectl create -f ./kubernetes/config/aline-secrets.yaml --namespace=dev
 kubectl create -f ./kubernetes/config/aline-configmap.yaml --namespace=dev
 kubectl create -f ./kubernetes/config/mysql-pv.yaml --namespace=dev
+kubectl create -f ./kubernetes/config/aline-ingress.yaml --namespace=dev
 kubectl create -f ./kubernetes/cluster --namespace=dev
