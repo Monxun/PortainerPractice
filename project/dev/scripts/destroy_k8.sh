530ba0badd77e7acdaaccf979549c5e43cf187e4
Monxun <michaelgibney1@gmail.com>
2022-03-14 02:33:48 -0700

    updated ingress

--- project/dev/scripts/destroy_k8.sh
+++ project/dev/scripts/destroy_k8.sh
@@ -3,7 +3,11 @@
 cd ..
 cd ansible
 kubectl delete -f ./kubernetes/cluster
-kubectl delete -f ./kubernetes/config
+kubectl delete -f ./kubernetes/config/aline-secrets.yaml --namespace=dev
+kubectl delete -f ./kubernetes/config/aline-configmap.yaml --namespace=dev
+kubectl delete -f ./kubernetes/config/mysql-pv.yaml --namespace=dev
+kubectl delete -f ./kubernetes/config/aline-ingress.yaml --namespace=dev
+
 
 cd ..
 cd scripts
