35eed120b0e71e73cbadb7c4163296325c50944b
Monxun <michaelgibney1@gmail.com>
2022-03-14 18:05:58 -0700

    update

--- project/dev/scripts/destroy_k8.sh
+++ project/dev/scripts/destroy_k8.sh
@@ -8,6 +8,5 @@ kubectl delete -f ./kubernetes/config/aline-configmap.yaml --namespace=dev
 kubectl delete -f ./kubernetes/config/mysql-pv.yaml --namespace=dev
 kubectl delete -f ./kubernetes/config/aline-ingress.yaml --namespace=dev
 
-
 cd ..
 cd scripts
