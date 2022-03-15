de54e5f02352ea86f86de30317dec852c4cb5923
Monxun <michaelgibney1@gmail.com>
2022-03-14 18:03:59 -0700

    update

--- project/dev/scripts/access-database.sh
+++ project/dev/scripts/access-database.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-kubectl run -it --rm --image=mysql:latest --restart=Never mysql-client -- mysql -h db-aline -proot --namespace=dev
+kubectl run -it --rm --image=mysql:latest --namespace=dev --restart=Never mysql-client -- mysql -h db-aline -proot 
 create database alinedb;
 create user 'alinedb' identified by 'alinedb';
 grant all privileges on alinedb . * to 'alinedb';
